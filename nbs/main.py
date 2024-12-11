2500e84f7deab32c8f19b71b208d293ff879dad2
abdelkareemkobo <kareem01095134688@gmail.com>
2024-12-11 05:14:19 +0200

    adding the fastapi wrapper to the mongodb

---
+++ nbs/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+import os
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from seo_rat.fast_gsc import app as search_api_app
+from seo_rat.store_gsc import store_router  # Import the storage router
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+app = FastAPI(
+    title="Search Console Analytics API",
+    description="API for Google Search Console Analytics",
+    version="1.0.0",
+)
+# add CORS middleware with specific origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost",
+        "http://localhost:8000",
+        "http://127.0.0.1",
+        "http://127.0.0.1:8000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include routes from search_api_app
+app.include_router(search_api_app.router)
+app.include_router(store_router)
+
+# Add callback URL configuration
+os.environ["OAUTH_CALLBACK_URL"] = "http://localhost:8000/callback"
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- sample/TODO.md
+++ sample/TODO.md
@@ -1,8 +1,9 @@
 ## Todo
 
-1. Clean teh test
+1. Clean the test
 2. Move the tests for 03_article into another test folder
-3. Focus keywords in Header
-4. Focus keyword not found in subheading(s) like H2,H3,H4..etc
-5. Add an Image with your Focus Keyword as alt text
-6. Keyword Density is 0.06 which is low, or hight
+3. Fix the quarto github publish deployement
+4. Focus keywords in Header
+5. Focus keyword not found in subheading(s) like H2,H3,H4..etc
+6. Add an Image with your Focus Keyword as alt text
+7. Keyword Density is 0.06 which is low, or hight
